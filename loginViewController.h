5c0390d01552e34bb3ad81fa523836be0461c2f5
Chad Heise <chad@heise.org>
2012-02-12 12:16:12 -0500

    Added login view

---
+++ loginViewController.h
@@ -0,0 +1,13 @@
+//
+//  loginViewController.h
+//  Inkle
+//
+//  Created by Chad Heise on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface loginViewController : UIViewController
+
+@end
