2129217e3bdb9659d12f21b3da84e3bddf22c4ec
Chad Heise <chad@heise.org>
2012-02-12 11:55:29 -0500

    Initial Commit

---
+++ InkleTests/InkleTests.h
@@ -0,0 +1,13 @@
+//
+//  InkleTests.h
+//  InkleTests
+//
+//  Created by Chad Heise on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface InkleTests : SenTestCase
+
+@end
---
+++ InkleTests/InkleTests.m
@@ -0,0 +1,32 @@
+//
+//  InkleTests.m
+//  InkleTests
+//
+//  Created by Chad Heise on 2/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "InkleTests.h"
+
+@implementation InkleTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in InkleTests");
+}
+
+@end
