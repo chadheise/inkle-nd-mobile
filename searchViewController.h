d2185a36bfaf433cada96302204fc2118a2b50b2
Julie Shawcross <Julie.wamser@gmail.com>
2012-02-13 12:09:32 -0500

    Define searchViewController

---
+++ searchViewController.h
@@ -0,0 +1,13 @@
+//
+//  searchViewController.h
+//  Inkle
+//
+//  Created by mobiapps on 2/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface searchViewController : UIViewController
+
+@end
