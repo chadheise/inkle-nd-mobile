97cae742de5b75a5c673b69564a4a39db00ab1d4
Chad Heise <chad@heise.org>
2012-02-18 17:38:25 -0500

    Reorganized storyboard after changing app navigation. Removed root tab controller and replaced with navigation controller.

---
+++ mainMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  mainMenuViewController.h
+//  Inkle
+//
+//  Created by Chad Heise on 2/18/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface mainMenuViewController : UIViewController
+
+@end
