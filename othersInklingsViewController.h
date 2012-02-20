dca0bc5a36d1c31c5063c9f491a9525307a96de6
Chad Heise <chad@heise.org>
2012-02-20 14:28:00 -0500

    Restructured to use tab view controller

---
+++ othersInklingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  othersInklingsViewController.h
+//  Inkle
+//
+//  Created by Chad Heise on 2/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface othersInklingsViewController : UIViewController
+
+@end
