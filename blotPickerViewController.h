e91d9703879a787118f5c98084cfacdd8dfa19eb
Julie Shawcross <Julie.wamser@gmail.com>
2012-03-29 17:57:00 -0400

    Created dependent picker! (hard coded populating arrays)

--- blotPickerViewController.h
+++ blotPickerViewController.h
@@ -7,16 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#define leftComponent 0
+#define rightComponent 1
 
 @interface blotPickerViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
 {
-    __weak UIPickerView *blotPicker;
+    __weak UIPickerView *picker;
     __weak UILabel *resultsLabel;
 }
 
-@property (weak, nonatomic) IBOutlet UIPickerView *blotPicker;
+@property (weak, nonatomic) IBOutlet UIPickerView *picker;
 @property (nonatomic, strong) NSMutableArray *blotNames;
 @property (nonatomic, strong) NSMutableArray *networkNames;
+@property (nonatomic, strong) NSMutableArray *categoryNames;
+@property (nonatomic, strong) NSMutableArray *namesArr;
 @property (weak, nonatomic) IBOutlet UILabel *resultsLabel;
+
+-(IBAction) buttonPressed;
 
 @end
--- blotPickerViewController.m
+++ blotPickerViewController.m
@@ -12,11 +12,31 @@
 @implementation blotPickerViewController {
     NSMutableArray *blotNames;
     NSMutableArray *networkNames;
+    NSMutableArray *categoryNames;
+    NSMutableArray *namesArr;
 }
-@synthesize blotPicker;
+@synthesize picker;
 @synthesize blotNames;
 @synthesize networkNames;
 @synthesize resultsLabel;
+@synthesize categoryNames;
+@synthesize namesArr;
+
+-(IBAction) buttonPressed {
+    NSInteger catRow = [picker selectedRowInComponent:leftComponent];
+    NSInteger popRow = [picker selectedRowInComponent:rightComponent];
+    
+    NSString *cat = [self.categoryNames objectAtIndex:catRow];
+    
+    peopleGroups *group = [self.namesArr objectAtIndex:popRow];
+    NSString *pop = group.name;
+    
+    NSString *title = [[NSString alloc] initWithFormat:@"You selected %@.",pop];
+    NSString *message = [[NSString alloc] initWithFormat:@"%@ is in %@", pop, cat];
+    
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:message delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    [alert show];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -36,22 +56,33 @@
   */
     self.blotNames = [NSMutableArray arrayWithCapacity:5];
     self.networkNames = [NSMutableArray arrayWithCapacity:5];
-    
+    self.categoryNames = [[NSMutableArray alloc] initWithObjects: @"Blots",@"Networks",nil];
+    self.namesArr = [NSMutableArray arrayWithCapacity:10];
     peopleGroups *blot = [[peopleGroups alloc]init];
     blot.pid = @"3";
     blot.name = @"Pizza Group";
     [blotNames addObject:blot];
+
     peopleGroups *network = [[peopleGroups alloc]init];
     network.pid = @"2";
     network.name = @"MIT Network";
     [networkNames addObject:network];
-    //blot = 
+    
+   /* network.pid = @"3";
+    network.name = @"";
+    [namesArr addObject:network];*/
+    
+   // NSString *selectedCat = [self.categoryNames objectAtIndex:0];
 }
 
 - (void)viewDidUnload
 {
-    [self setBlotPicker:nil];
+    [self setPicker:nil];
     [self setResultsLabel:nil];
+    self.categoryNames = nil;
+    self.blotNames = nil;
+    self.networkNames = nil;
+    self.namesArr = nil;
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -67,19 +98,26 @@
 - (NSInteger)numberOfComponentsInPickerView:
 (UIPickerView *)pickerView
 {
-    return 1;
+    return 2;
 }
 - (NSInteger)pickerView:(UIPickerView *)pickerView
 numberOfRowsInComponent:(NSInteger)component
 {
-    return [blotNames count];
+    if (component == leftComponent)
+        return [self.categoryNames count];
+    return [self.namesArr count];
+ //   return [blotNames count];
 }
 - (NSString *)pickerView:(UIPickerView *)pickerView
              titleForRow:(NSInteger)row
             forComponent:(NSInteger)component
 {
-    peopleGroups *blot = [blotNames objectAtIndex:row];
-    return blot.name;
+    //peopleGroups *blot = [blotNames objectAtIndex:row];
+    //return blot.name;
+    if(component == leftComponent)
+        return [self.categoryNames objectAtIndex:row];
+    peopleGroups *group = [namesArr objectAtIndex:row];
+    return group.name;
 } 
 
 #pragma mark -
@@ -87,10 +125,20 @@ numberOfRowsInComponent:(NSInteger)component
 -(void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row
       inComponent:(NSInteger)component
 {
-    peopleGroups *blot = [blotNames objectAtIndex:row];
-    resultsLabel.text = blot.name;
+    if (component == leftComponent){
+        NSString *selectedCat = [self.categoryNames objectAtIndex:row];
+        if (selectedCat == @"Blots")
+            self.namesArr = self.blotNames;
+        else if(selectedCat == @"Networks")
+            self.namesArr = self.networkNames;
+        [picker selectRow:0 inComponent:rightComponent animated:YES];
+        [picker reloadComponent:rightComponent];
+        
+    }
+   // peopleGroups *blot = [blotNames objectAtIndex:row]; previous #2
+   // resultsLabel.text = blot.name;
     
-    //resultsLabel.text = [NSString stringWithFormat: @"%@", [blotNames objectAtIndex:row]];
+    //resultsLabel.text = [NSString stringWithFormat: @"%@", [blotNames objectAtIndex:row]]; previous #1
 }
 
 
