8d3c6ce9d66ff3706bb1b5f075dfaee6c79dcccf
Chad Heise <chad@heise.org>
2012-04-23 12:15:12 -0400

    My Inklings page loads web view content and does not scale

--- SetMyInklingViewController.m
+++ SetMyInklingViewController.m
@@ -28,6 +28,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.myInklingWebView.scalesPageToFit = NO;
     [self.myInklingWebView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.inkleit.com/mobile/setMyInkling"]]];
 }
 
