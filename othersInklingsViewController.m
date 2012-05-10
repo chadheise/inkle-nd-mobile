b1b07d594596b6bc504d546533f251b98fa97085
Julie Shawcross <Julie.wamser@gmail.com>
2012-05-10 12:44:27 -0400

    d

--- othersInklingsViewController.m
+++ othersInklingsViewController.m
@@ -174,20 +174,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-                      
-    inklingDate = [NSDate date]; //Initialize inklingDate to today
     
-    OthersInklingsDate *theAppDataObject2 = [self theAppDataObject2];
+    //Initialize inklingDate to today
+    inklingDate = [NSDate date]; 
     
     peopleType = @"network";
     peopleId = @"1";
     inklingType = @"all";
     [self updateInklings];
     
+    //Update the date button
+    OthersInklingsDate *theAppDataObject2 = [self theAppDataObject2];
     [dateButton setTitle:theAppDataObject2.dateString forState:UIControlStateNormal];
 
 }
-
 
 - (void)viewDidUnload
 {
@@ -246,45 +246,6 @@
     return cell;
     
 }
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
- {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
- }   
- else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }   
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
- {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
- {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
 
 #pragma mark - Table view delegate
 
